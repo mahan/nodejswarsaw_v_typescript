2a3a4ff4278b93f976ea49ff88f29910c8fc2fba
Mattias Hansson <mattias@techserio.com>
2021-11-23 19:11:31 +0100

    First project commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+
+import express from 'express';
+
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+
+  res.send('hi!');
+
+});
+
+app.listen(PORT);
+console.log(`started on port ${PORT}`)
